5aeaa1c4c3403360bf86ad6bb1c660fbad4fdaba
Clément Ghnassia <clement.ghnassia@sightcall.com>
2015-02-09 20:35:26 -0800

    First Commit

--- src/poker.js
+++ src/poker.js
@@ -1,7 +1,19 @@
 var poker = {
   // Returns the name of the input hand.
   // See the specs for the required formats.
+  var Card = function(cardString) {
+  	this.rank = cardString[0];
+  	this.suit = cardString[1];
+  }
+
+  card.prototype.myFunnction = function () {
+
+  };
+
   labelHand: function(hand) {
 
+  	cards = hand.split(' ').map(function(cardString) {
+  		return Card(cardString);
+  	});
   }
 };
